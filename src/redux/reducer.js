e9f7e99869b2c87e222a746264b45d834de640c9
anil sidhu <webanilsidhu@gmail.com>
2022-06-19 01:22:29 +0530

    Done with switch statment

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,12 +3,19 @@ import { ADD_TO_CART } from "./constant"
 
 
 export const cartData = (data = [], action) => {
-    if (action.type === ADD_TO_CART) {
-    console.warn("ADD_TO_CART condtion ", action)
+    // if (action.type === ADD_TO_CART) {
+    //     console.warn("ADD_TO_CART condition ", action)
 
-        // some logic
-        return data
-    } else {
-        return "no action called"
+    //     // some logic
+    //     return data
+    // } else {
+    //     return "no action called"
+    // }
+    switch (action.type) {
+        case ADD_TO_CART:
+            console.warn("ADD_TO_CART condition ", action)
+            return 1 + 1
+        default:
+            return "no action called"
     }
 }
